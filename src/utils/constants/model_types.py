bd79a8515d8c5b06555c6f550c544f53d94645d2
Liam Attard <liam@pop-os.localdomain>
2023-01-14 16:38:55 +0100

    Added top 20 model

--- src/utils/constants/model_types.py
+++ src/utils/constants/model_types.py
@@ -8,6 +8,7 @@ class Model_Type(enum.Enum):
     game_net_item_coll = "GameNet w Item based Collaborative Model"
     game_net_age_item_coll = "GameNet w Item based Collaborative Model with age"
     final_model = "HR, Age, Diag, Pro as input"
+    top_20 = "Model that recommends the top 20 most popular medicine"
 
     #Deprecated
     pure_collaborative = "Pure Collaborative Model"
