ae86950e7fed52cd211432fc60d513eae0163a09
Liam Attard <liam@pop-os.localdomain>
2023-02-05 12:59:55 +0100

    Forgot to push start.py

--- src/model_trainer/start.py
+++ src/model_trainer/start.py
@@ -2,16 +2,15 @@ import src.model_trainer.gamenet_train as gamenet_train
 import src.model_trainer.gamenet_realistic_train as gamenet_realistic_train
 import src.model_trainer.top_20_realistic_train as gamenet_realistic_train_top
 from src.utils.constants.model_types import Model_Type
-from src.utils.constants.dataset_types import Dataset_Type
 import src.utils.tools as tools
 
-def start(model_type, dataset, dataset_type, wandb, features = None):
+def start(dataset, dataset_type, wandb, features = None, threshold=None, epochs = None, model_type = None):
 
     if model_type == Model_Type.top_20:
         gamenet_realistic_train_top.train(dataset, dataset_type, model_type, wandb)
 
     elif tools.isByDate(dataset_type):
-        gamenet_realistic_train.train(dataset, dataset_type, model_type, wandb, features)
+        gamenet_realistic_train.train(dataset, dataset_type, wandb, features, threshold, epochs)
 
     elif model_type == Model_Type.game_net_age:
         gamenet_train.train(dataset, dataset_type, model_type, wandb, with_age=True)
@@ -21,4 +20,5 @@ def start(model_type, dataset, dataset_type, wandb, features = None):
 
 def test(path, model_type, dataset):
     # gamenet_train.test(path, dataset)
-    gamenet_realistic_train.test(path, model_type, dataset)
+    #gamenet_realistic_train.test(path, model_type, dataset)
+    print("hello")
